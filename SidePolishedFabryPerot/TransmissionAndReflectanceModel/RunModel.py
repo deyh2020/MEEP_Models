dc03db51462a16dd78c859fa5efe3a4982431ba1
Chriscrossing <cjohnsonrichards@gmail.com>
2021-11-05 13:32:42 +0000

    lol

--- SidePolishedFabryPerot/TransmissionAndReflectanceModel/RunModel.py
+++ SidePolishedFabryPerot/TransmissionAndReflectanceModel/RunModel.py
@@ -23,23 +23,23 @@ Model.buildPolished()
 #do normalisation run
 #build model without defects
 
-#Model.BuildModel(NormRun=False,Plot=False) 
-#Model.NormRun()
+Model.BuildModel(NormRun=False,Plot=False) 
+Model.NormRun()
 
 
 
 #resetMEEP
-#Model.sim.reset_meep()
+Model.sim.reset_meep()
 
 
 #Build model with everything.
 Model.addtriBubbles()
-Model.BuildModel(NormRun=False,Plot=True) 
+Model.BuildModel(NormRun=False,Plot=False) 
 
 # for normal run, load negated fields to subtract incident from refl. fields
-#Model.sim.load_minus_flux_data(Model.refl, Model.norm_refl)
+Model.sim.load_minus_flux_data(Model.refl, Model.norm_refl)
 
 #Run until fields decayed by 1e3
-#Model.AutoRun()
+Model.AutoRun()
 
 
