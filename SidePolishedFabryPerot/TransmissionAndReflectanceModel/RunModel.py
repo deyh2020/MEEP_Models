461ec8c276d835c7ed1e7d312ac6aa9c3e86b4d2
Chriscrossing <cjohnsonrichards@gmail.com>
2021-11-05 10:03:18 +0000

    longer runtime

--- SidePolishedFabryPerot/TransmissionAndReflectanceModel/RunModel.py
+++ SidePolishedFabryPerot/TransmissionAndReflectanceModel/RunModel.py
@@ -11,7 +11,7 @@ Model.fcen   = 1/1.55
 Model.df     = 1.3e-2
 Model.nfreq  = 1000
 Model.res = 5
-Model.DecayF = 1e-3
+Model.DecayF = 1e-10
 Model.filename = 'TopsyTestRes5'
 Model.Notes    = ''
 
@@ -23,23 +23,23 @@ Model.buildPolished()
 #do normalisation run
 #build model without defects
 
-Model.BuildModel(NormRun=False,Plot=False) 
-Model.NormRun()
+#Model.BuildModel(NormRun=False,Plot=False) 
+#Model.NormRun()
 
 
 
 #resetMEEP
-Model.sim.reset_meep()
+#Model.sim.reset_meep()
 
 
 #Build model with everything.
 Model.addtriBubbles()
-Model.BuildModel(NormRun=False,Plot=False) 
+Model.BuildModel(NormRun=False,Plot=True) 
 
 # for normal run, load negated fields to subtract incident from refl. fields
-Model.sim.load_minus_flux_data(Model.refl, Model.norm_refl)
+#Model.sim.load_minus_flux_data(Model.refl, Model.norm_refl)
 
 #Run until fields decayed by 1e3
-Model.AutoRun()
+#Model.AutoRun()
 
 
