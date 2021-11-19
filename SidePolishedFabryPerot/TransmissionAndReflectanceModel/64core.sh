8822ef59431962ccf3ae89fc9b2a8acd93f6b592
chriscrossing <cjohnsonrichards@gmail.com>
2021-11-19 12:24:46 +0000

    64core

--- SidePolishedFabryPerot/TransmissionAndReflectanceModel/64core.sh
+++ SidePolishedFabryPerot/TransmissionAndReflectanceModel/64core.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
-#SBATCH -t 01:00:00          
+#SBATCH -t 02:00:00          
 #SBATCH --ntasks=64
 
 module load meep
 
  
-srun python3 RunModel.py
+srun python3 RunBulkExperiment.py
 
 
