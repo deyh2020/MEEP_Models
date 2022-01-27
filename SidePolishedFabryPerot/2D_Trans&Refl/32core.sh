a480ad8de935f615d1ca48ad40d312217d2a90fa
ChriscrossingThinkpad <cjohnsonrichards@gmail.com>
2022-01-27 10:41:48 +0000

    topsy file

--- SidePolishedFabryPerot/2D_Trans&Refl/32core.sh
+++ SidePolishedFabryPerot/2D_Trans&Refl/32core.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
-#SBATCH -t 6:00:00          
+#SBATCH -t 3:00:00          
 #SBATCH --ntasks=32
 
 module load meep
 
  
-srun python3 PolishedFibre.py
-
-srun python3 WholeFibre.py
-
+srun python3 FullDevice.py
 
