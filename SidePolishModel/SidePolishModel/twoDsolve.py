8684e0b1831c44ad8ac2fd5c84eb39d902a013df
ChristianThinkpad <cjohnsonrichards@gmail.com>
2022-03-08 12:36:02 +0000

    updated experiment

--- SidePolishModel/SidePolishModel/twoDsolve.py
+++ SidePolishModel/SidePolishModel/twoDsolve.py
@@ -1,6 +1,14 @@
 import TwoDimensionTR.TwoDsolve as TwoDsolve
+import sys
 
+print ('Argument List:', str(sys.argv))
 
 Solver = TwoDsolve.TwoDsolve()
+
+try:
+    Solver.M.GAP = float(sys.argv[1])
+except:
+    print("That didn't work")
+
 
 Solver.run()
---
+++ SidePolishModel/bin/500umGAP.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+#SBATCH -t 05:00:00         
+#SBATCH --ntasks=32
+
+module load meep
+
+srun python3 ../SidePolishModel/twoDsolve.py 500 
