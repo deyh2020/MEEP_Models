c3b4d0df1b1cde4b21a8d3ef990bacb2b18d91c4
ChristianThinkpad <cjohnsonrichards@gmail.com>
2022-03-10 10:40:38 +0000

    new exp

--- SidePolishModel/bin/1mmGAP.sh
+++ SidePolishModel/bin/1mmGAP.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH -t 10:00:00         
-#SBATCH --ntasks=32
+#SBATCH --ntasks=48
 
 module load meep
 
-srun python3 ../SidePolishModel/twoDsolve.py 1000 2D_1mm
+srun python3 ../SidePolishModel/twoDsolve.py 1000 2D_1mm_PAD-1000
